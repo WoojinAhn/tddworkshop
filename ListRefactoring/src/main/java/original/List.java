b83fee84759df5b5d40a1d997e6de62784af4c12
Jaehoon Oh <ojh420@gmail.com>
2018-09-18 22:17:10 +0900

    Rollback original

--- ListRefactoring/src/main/java/original/List.java
+++ ListRefactoring/src/main/java/original/List.java
@@ -1,40 +1,26 @@
 package original;
 
 public class List {
-	public static final int INITIAL_STORE_SIZE = 10;
-	public static final int STORE_INCREMENT = 10;
-	private Object[] elements = new Object[INITIAL_STORE_SIZE];
+	private Object[] elements = new Object[10];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (readOnly) {
-			return;
+		if (!readOnly) {
+			int newSize = size + 1;
+
+			if ( newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + 10];
+				for (int i = 0; i < size; i++) {
+					newElements[i] = elements[i];
+				}
+
+				elements = newElements;
+			}
+
+			elements[size] = element;
+			size++;
 		}
-
-		if (isFull()) {
-			increaseElementStore();
-        }
-
-		addElementAtEnd(element);
-	}
-
-	private void addElementAtEnd(Object element) {
-		elements[size] = element;
-		size++;
-	}
-
-	private void increaseElementStore() {
-		Object[] newElements = new Object[elements.length + STORE_INCREMENT];
-		for (int i = 0; i < size; i++) {
-            newElements[i] = elements[i];
-        }
-
-		elements = newElements;
-	}
-
-	private boolean isFull() {
-		return size + 1 > elements.length;
 	}
 
 	public int size() {
